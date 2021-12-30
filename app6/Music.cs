7436a2c50f42fa4a993f94473432d41db0e05d5d
colmendean123 <68941235+colmendean123@users.noreply.github.com>
2021-12-29 23:30:37 -0800

    Add files via upload

---
+++ app6/Music.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace app6
+{
+     public class Music
+    {
+        public byte[] myData { get; set; }
+        public byte[] get()
+        {
+            return myData;
+        }
+
+      public Music()
+        {
+
+        }
+    }
+}
